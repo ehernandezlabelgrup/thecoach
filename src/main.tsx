6ec77a527ac6a3963a5dd9c4ffcf0483363bff35
Emilio Hernandez <eherna40@gmail.com>
2023-12-02 15:29:09 +0100

    dev

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,7 @@
-
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+/* eslint-disable react-refresh/only-export-components */
+import App from './App'
 import './index.css'
-// import React from 'react'
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-class MyReactAppElement extends HTMLElement {
-  constructor() {
-    super()
-  }
+import r2wc from "@r2wc/react-to-web-component"
 
-  connectedCallback() {
-    this.renderReactComponent()
-  }
-
-  renderReactComponent() {
-    const container = document.createElement("div")
-    this.appendChild(container)
-    ReactDOM.createRoot(container).render(<App />)
-  }
-}
-customElements.define("the-coach", MyReactAppElement)
+const WebGreeting = r2wc(App)
+customElements.define("web-greeting", WebGreeting)
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    // build: {
-    //   lib: {
-    //     entry: './src/AppWebComponent.ts', // Cambia esto por la ruta de tu archivo principal
-    //     name: 'MyComponent',
-    //     formats: ['es'], // Para ESM
-    //   }
-    // }
 })
