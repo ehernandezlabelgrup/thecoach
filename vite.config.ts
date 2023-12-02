7515ada60106f2ba5bdaeb48a9f86e5cbc619e2e
Emilio Hernandez <eherna40@gmail.com>
2023-12-02 15:55:52 +0100

    'deploy'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`, // elimina el hash para archivos JS
+        chunkFileNames: `assets/[name].js`, // elimina el hash para chunks JS
+        assetFileNames: `assets/[name].[ext]`, // elimina el hash para otros assets como CSS
+      },
+    },
+  },
+});
